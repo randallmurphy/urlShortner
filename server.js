f0bb3eaa840566bdae199a6cabc53d0ab3c3564c
randall murphy <rmurphy@perseverenow.org>
2025-04-11 11:26:38 -0500

    another commit dif mongoose setup and post

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const shortUrl = require('./models/shortUrl');
 
 // middleware
@@ -116,35 +116,35 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 // connect to database 
- //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shortUrls', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rmurphy:HumblePie!3@cluster0.pbswn.mongodb.net/?appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+ mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://rmurphy:HumblePie!3@cluster0.pbswn.mongodb.net/?appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://rmurphy:HumblePie!3@cluster0.pbswn.mongodb.net/?appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
